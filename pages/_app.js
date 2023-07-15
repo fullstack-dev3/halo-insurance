12384d914ee2232b0c1fa95fde28885cd38f8e2b
full-stack-david <davidchan12323@gmail.com>
2023-07-15 10:02:35 -0500

    Add all page layouts

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
 import '../styles/globals.css'
 import Image from 'next/image'
+import Layout from '../components/layout'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
